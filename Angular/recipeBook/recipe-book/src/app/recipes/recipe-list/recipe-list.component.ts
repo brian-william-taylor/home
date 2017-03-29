f2f649fcbfa41855c4272ed560adb94b363d6525
brian-william-taylor <testing@utmuser174-142.wireless.utoronto.ca>
2017-03-29 17:18:48 -0400

    Added Routing to application

--- Angular/recipeBook/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/recipeBook/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,27 +1,26 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {Recipe} from '../recipe';
 
 import { RecipeItemComponent} from './recipe-item.component';
+
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes : Recipe[] = [
-    new Recipe('Schnitzel', 'Dummy Description', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', []),
-    new Recipe('Summer Salad', 'Dummy Description 2', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', []),
-  ];
-  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [];
+  
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { 
+
+  }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onSelected(recipe: Recipe){
-    this.recipeSelected.emit(recipe);
-  }
 
 }
