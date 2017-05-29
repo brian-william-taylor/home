a83a9c20593a3dd127678a43244358e2741394eb
Brian Taylor <taylor.brian93@gmail.com>
2017-05-29 09:03:01 -0400

    implemented jwt on the backend for authentication.

--- React/Auth/server/controllers/authentication.js
+++ React/Auth/server/controllers/authentication.js
@@ -8,6 +8,12 @@ function tokenForUser(user){
     return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next){
+    // User has been authenticated, just need a password now
+    // We just need to have the user, passed from passport via done 
+    res.send({ token: tokenForUser(req.user)});
+}
+
 exports.signup = function(req, res, next){
     const email = req.body.email;
     const password = req.body.password;
