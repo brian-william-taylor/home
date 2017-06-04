f427e065279a591c16ac1fece39026d1c9b3e497
Brian Taylor <taylor.brian93@gmail.com>
2017-06-03 21:21:49 -0400

    set up dynamic header. Handle bad login error. Retrieve and store jwt token.

--- React/Auth/frontend/src/components/auth/signin.js
+++ React/Auth/frontend/src/components/auth/signin.js
@@ -1,34 +1,56 @@
-import React, { Component} from 'react';
-import { reduxForm } from 'redux-form';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import {connect} from 'react-redux';
 
-
+import * as actions from '../../actions';
+ 
+const renderInput = field =>
+  <div>
+    <input {...field.input} type={field.type} className="form-control" />
+  </div>
+ 
 class Signin extends Component {
+ 
+  handleFormSubmit({ email, password}){
+    console.log(email, password)
+    this.props.signinUser({email, password});
+  }
 
-    handleFormSubmit({ email, password }){
-        console.log( email, password );
-        // Need to do something to log user in
-    }
 
-    render(){
-        const {handleSubmit, fields: { email, password}} = this.props;
-
-        return (
-            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <fieldset className="form-group">
-                    <label>Email:</label>
-                    <input {...email} className="form-control" />
-                </fieldset>
-                <fieldset className="form-group">
-                    <label>password</label>
-                    <input {...password} className="form-control" />
-                </fieldset>
-                <button action="submit" className="btn btn-primary">Sign In</button>
-            </form>
-        );
-    }
+  renderAlert(){
+      if(this.props.errorMessage){
+          return (
+              <div className="alert alert-danger">
+                  <strong>Oops!</strong> {this.props.errorMessage}
+             </div>
+          )
+      }
+  }
+ 
+  render(){
+  const { handleSubmit } = this.props
+ 
+    return(
+    <form onSubmit={ handleSubmit(this.handleFormSubmit.bind(this)) }>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <Field name="email" component={renderInput} type="text" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Email</label>
+        <Field name="password" component={renderInput} type="password" />
+      </div>
+      {this.renderAlert()}
+      <button action="submit" className="btn btn-primary">Sign in</button>
+    </form>
+    )
+  }
 }
 
-export default reduxForm({
-    form: 'signin', 
-    fields: ['email', 'passowrd']
-})(Signin);
+function mapStateToProps(state){
+    return { errorMessage: state.auth.error}
+}
+ 
+Signin =  reduxForm({ form: 'signin' })(Signin)
+
+export default connect(mapStateToProps, actions)(Signin);
